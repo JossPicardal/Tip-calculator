a7921378a794ccfd2cb593c8330103952367176f
Das Ist Die ウィーブボイ Кто любит кодировать <57271278+JossPicardal@users.noreply.github.com>
2020-02-03 09:01:03 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 //Calculate Tip
 function calculateTip() {
-  var billAmt = document.getElementById("bill_amt").value;
-  var serviceQual = document.getElementById("Quallity").value;
-  var numOfPeople = document.getElementById("people").value;
+  var bill_Amt = document.getElementById("bill_Amt").value;
+  var Quality = document.getElementById("Quality").value;
+  var people = document.getElementById("people").value;
 
   //validate input
-  if (billAmt === "" || serviceQual == 0) {
+  if (bill_Amt === "" || Quality == 0) {
     alert("Please enter values");
     return;
   }
   //Check to see if this input is empty or less than or equal to 1
-  if (numOfPeople === "" || numOfPeople <= 1) {
-    numOfPeople = 1;
+  if (people === "" || people <= 1) {
+    people = 1;
     document.getElementById("each").style.display = "none";
   } else {
     document.getElementById("each").style.display = "block";
   }
 
   //Calculate tip
-  var total = (bill_Amt * serviceQual) / numOfPeople;
+  var total = (bill_Amt * Quality) / people;
   //round to two decimal places
   total = Math.round(total * 100) / 100;
   //next line allows us to always have two digits after decimal point

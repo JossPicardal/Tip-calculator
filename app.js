02209d67c3b76fe197cb55e0429d7602ffd189b8
Das Ist Die ウィーブボイ Кто любит кодировать <57271278+JossPicardal@users.noreply.github.com>
2020-02-05 10:57:32 -1000

    almost last commit

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ function calculateTip() {
   var qual = document.getElementById("qual").value;
   var people = document.getElementById("people").value;
 
-  //validate input
-  if (bill_Amt === "" ) {
+  //easter eggs
+  if (bill_Amt === "" ||people == 0) {
     alert("PUT SOMETHING IN YA DIMWIT!!");
     return;
-  }else (qual == 0){
+  }
+  if (qual == 0){
     alert("If the service was that terrible WHY ARE YOU GIVING THEM A TIP")
     return
   }
+  if (bill_Amt === "uwu" ) {
+    alert("Okay, I know this is a really bad idea but /n Im already here so /n Here we fuckin’ go /n Rawr /n /n x3 nuzzles, pounces on you, uwu you so warm /n Couldnt help but notice your bulge from across the floor /n Nuzzles your necky wecky-tilde murr-tilde, hehe /n Unzips your baggy ass pants, oof baby you so musky /n Take me home, pet me, and make me yours and dont forget to stuff me /n See me wag my widdle baby tail all for your buldgy-wuldgy /n Kissies and lickies your neck /n I hope daddy likies /n Nuzzles and wuzzles your chest /n I be  gettin’ thirsty /n /n Hey, I got a little itch, you think you can help me? /n Only seven inches long, uwu, please adopt me /n Paws on your buldge as I lick my lips /n Bout to hit em with this furry shit ")
+    document.body.style.backgroundImage = "url('https://steamuserimages-a.akamaihd.net/ugc/939446042746783339/397B115E5FF3FE0D1034C20E36063D631A5C4002/')";
+    return
+  }
+  if (bill_Amt === "420"){
+    document.body.style.backgroundImage = "url('https://media0.giphy.com/media/WIQBk5fKfHvyM/source.gif')";
+  }
+
+  if (bill_Amt === "563923" ) {
+    alert("COMRADE THE TIME HAS COME PREPARE FOR WAR")
+    document.getElementById('')
+  }
+
   //Check to see if this input is empty or less than or equal to 1
   if (people === "" || people <= 1) {
     people = 1;
